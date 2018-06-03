af91d161b2657298cbba06c5e74bc6b75f4f238d
Thomas <mcdonnellthomas89@gmail.com>
2018-06-03 16:51:21 +0100

    memoization addition

--- language_python/brute_force_knapsack.py
+++ language_python/brute_force_knapsack.py
@@ -43,14 +43,52 @@ def max_value(items_to_consider, available_weight):
 
 
 """
+Modification of max_value function to add memoization for problem sets with
+overlapping subproblems.
+"""
+
+
+def fast_max_value(items_to_consider, available_weight, memo = {}):
+    # len(items_to_consider) => this is a compact way of representing the items
+    # left to be considered as items are removed from the front and so by
+    # knowing the lenght we by default know the items remaining. 
+    if (len(items_to_consider), available_weight) in memo:
+        result = memo[(len(items_to_consider), available_weight)]
+    elif items_to_consider == [] or available_weight == 0:
+        result = (0, ())
+    elif items_to_consider[0].get_weight() > available_weight:
+        #Explore right branch only
+        result = fast_max_value(items_to_consider[1:], available_weight, memo)
+    else:
+        next_item = items_to_consider[0]
+        #Explore left branch
+        with_value, with_to_take = fast_max_value(items_to_consider[1:],
+                                     available_weight - next_item.get_weight(),
+                                            memo)
+        with_value += next_item.get_value()
+        #Explore right branch
+        without_value, without_to_take = fast_max_value(items_to_consider[1:],
+                                                   available_weight, memo)
+        #Choose better branch
+        if with_value > without_value:
+            result = (with_value, with_to_take + (next_item,))
+        else:
+            result = (without_value, without_to_take)
+    memo[(len(items_to_consider), available_weight)] = result
+    return result
+
+
+
+
+"""
 ************************************TEST UNIT ******************************************
 """
 
 
-def test_max_value(items, constraint, print_items=True):
-    value, taken = max_value(items, constraint)
-    print(f'Total value of items take is {value}')
+def test(items, constraint, algorithm, print_items=True):
+    value, taken = algorithm(items, constraint)
     if print_items:
+        print(f'Total value of items take is {value}')
         for item in taken:
             print(f'{item}')
 
@@ -60,4 +98,5 @@ if __name__ == '__main__':
     values = [10, 100, 10, 15, 450, 500, 125]
     weights = [100, 100, 200, 300, 1230, 1000, 100]
     collection = build_items(items, values, weights)
-    test_max_value(collection, 750)
+    test(collection, 750, max_value)
+    test(collection, 750, fast_max_value)
