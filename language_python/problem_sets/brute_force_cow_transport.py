a57dbb720654c4c4edc780d44c013df9aa92cffd
Thomas <mcdonnellthomas89@gmail.com>
2018-07-16 19:51:53 +0100

    brute_force set partition function

---
+++ language_python/problem_sets/brute_force_cow_transport.py
@@ -0,0 +1,26 @@
+#From codereview.stackexchange.com                    
+def partitions(set_):
+    if not set_:
+        yield []
+        return
+    for i in range(2**len(set_)//2):
+        parts = [set(), set()]
+        for item in set_:
+            parts[i&1].add(item)
+            i >>= 1
+        for b in partitions(parts[1]):
+            yield [parts[0]]+b
+
+
+# This is a helper function that will fetch all of the available 
+# partitions for you to use for your brute force algorithm.
+def get_partitions(set_):
+    for partition in partitions(set_):
+        yield [list(elt) for elt in partition]
+
+
+def brute_force_transport(cows, limit=10):
+    pass
+
+if __name__ == "__main__":
+    main()
